9f07bf913e38e3dba2306e8cafbae28b6c650717
Samir Jadhav <samirzjadhav@gmail.com>
2024-03-06 10:14:34 +0530

    feat: added their message section

--- src/Components/TheirMessage.js
+++ src/Components/TheirMessage.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const TheirMessage = () => {
-  return <div>Their Message</div>;
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avater"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
+      )}
+
+      {message.attachments && message.attachments.length > 0  (
+           <img
+           src={message.attachment[0].file}
+           className="message-image"
+           alt="message-attachment"
+           style={{ float: "right" }}
+         />
+      )}
+    </div>
+  );
 };
 
 export default TheirMessage;
