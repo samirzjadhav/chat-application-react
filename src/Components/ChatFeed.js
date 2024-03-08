cf8a4e7ac92989ee3a54867b2eb2ac9212babc92
Samir Jadhav <samirzjadhav@gmail.com>
2024-03-08 11:31:12 +0530

    feat: added render read receipts for message and ismymessage

--- src/Components/ChatFeed.js
+++ src/Components/ChatFeed.js
@@ -6,6 +6,20 @@ const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
 
   const chat = chats && chats[activeChat];
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map((person, index) => {
+      person.last_read === message.id && (
+        <div
+          key={`read_${index}`}
+          className="read-receipt"
+          style={{
+            float: isMyMessage ? "right" : "left",
+            backgroundImage:
+              person.person.avatar && `url(${person.person.avatar})`,
+          }}
+        ></div>
+      );
+    });
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
@@ -32,7 +46,9 @@ const ChatFeed = (props) => {
               marginRight: isMyMessage ? "18px" : "0px",
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
-          ></div>
+          >
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
         </div>
       );
     });
